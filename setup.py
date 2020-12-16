825c5832551a77560fa1ff59a5831491d07368f5
Joe Heindel <heindelj@uw.edu>
2020-12-16 15:21:30 -0800

    tweaked setup

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='py_MD',  
-     version='0.1',
+     version='0.2',
      author="Joseph Heindel",
      author_email="heindelj@uw.edu",
      description="A molecular dynamics package in python",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/heindelj/pyMD",
+     download_url = 'https://github.com/heindelj/pyMD/archive/v_02.tar.gz',
      install_requires=['numpy', 'tidynamics'],
      packages=setuptools.find_packages(),
      classifiers=[

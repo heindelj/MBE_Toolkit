34dcc0286311b68d233352d1dd648ebcf0189dad
Joe Heindel <heindelj@uw.edu>
2020-12-16 14:57:01 -0800

    updated init file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='py-MD',  
+     name='py_MD',  
      version='0.1',
      author="Joseph Heindel",
      author_email="heindelj@uw.edu",
